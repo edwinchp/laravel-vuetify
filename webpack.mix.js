a698debcbc69f2defd7fa695b4d5fe27eb2788f2
Edwin Chi Pineiro <chipineiro@gmail.com>
2022-03-17 07:45:26 -0600

    fix when installing Vue. Vuetify installed git ignore updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,15 @@
 const mix = require('laravel-mix');
 
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+var webpackConfig = {
+    plugins: [
+        new VuetifyLoaderPlugin(),
+    ]
+}
+
+mix.webpackConfig(webpackConfig);
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +22,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .sass('resources/sass/app.scss', 'public/css');
